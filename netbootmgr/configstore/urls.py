61a9bf1a35b5b2ee293f4db20702a7b1ecbf535f
Dominic S <git@dominic86.de>
2016-07-18 01:13:00 +0200

    adding the ability to find host configurations based on uuid or mac address

--- netbootmgr/configstore/urls.py
+++ netbootmgr/configstore/urls.py
@@ -3,8 +3,11 @@ from django.conf.urls import url
 from netbootmgr.configstore import views
 
 urlpatterns = [
-    url(r'^name/(?P<cfg_name>[^/]+)/host/(?P<host_id>[^/]+)', views.get_config, name='cfg'),
-    url(r'^name/(?P<cfg_name>[^/]+)', views.get_config, name='cfg'),
+    url(r'^(?P<cfg_name>[^/]+)/host/uuid/(?P<uuid>[^/]+)', views.get_config, name='cfg'),
+    url(r'^(?P<cfg_name>[^/]+)/host/mac/(?P<mac_address>[^/]+)', views.get_config, name='cfg'),
+    url(r'^(?P<cfg_name>[^/]+)/host/uuid/(?P<uuid>[^/]+)/mac/(?P<mac_address>[^/]+)', views.get_config, name='cfg'),
+    url(r'^(?P<cfg_name>[^/]+)/host/(?P<host_id>[^/]+)', views.get_config, name='cfg'),
+    url(r'^(?P<cfg_name>[^/]+)', views.get_config, name='cfg'),
     url(r'^id/(?P<cfg_id>[^/]+)/host/(?P<host_id>[^/]+)', views.get_config, name='cfg'),
     url(r'^id/(?P<cfg_id>[^/]+)', views.get_config, name='cfg'),
     url(r'^$', views.index, name='index'),
--- netbootmgr/configstore/views.py
+++ netbootmgr/configstore/views.py
@@ -6,14 +6,12 @@ from netbootmgr.bootmgr.models import SiteConfig
 from django.shortcuts import get_object_or_404
 
 
-def get_config(request, cfg_name=None, cfg_id=None, host_id=None):
+def get_config(request, cfg_name=None, cfg_id=None, host_id=None, uuid=None, mac_address=None):
 
     try:
         site_config = SiteConfig.get_from_request(request)
     except SiteConfig.DoesNotExist:
         raise Http404('SiteConfig not found.')
-
-    print(site_config)
 
     if host_id and re.compile('^\d+$').match(host_id) is None:
         raise Http404('Host ID is invalid.')
@@ -23,6 +21,14 @@ def get_config(request, cfg_name=None, cfg_id=None, host_id=None):
 
     if host_id:
         host = get_object_or_404(Host, pk=host_id)
+    elif uuid or mac_address:
+        identifiers = []
+        if uuid:
+            identifiers.append({'identifier_type': 'uuid', 'identifier': uuid})
+        if mac_address:
+            identifiers.append({'identifier_type': 'mac', 'identifier': mac_address})
+
+        host = Host.get_from_unique_identifiers(identifiers)
     else:
         host = None
 
